6767ed5d9d59d758042aa1fa9c91df1d37ec7d20
TonyWu20 <tony.w21@gmail.com>
2022-06-02 00:20:08 +0800

    Perform task_gen_all

--- gdy_basic_model/src/main.rs
+++ gdy_basic_model/src/main.rs
@@ -1,31 +1,13 @@
 extern crate gdy_model;
 extern crate nalgebra as na;
+mod editor;
 mod parser;
+mod test;
 fn main() {
-    println!("Hello World!");
+    task_gen_all();
 }
 
-#[test]
-fn test_reg() {
-    use gdy_model::*;
-    use na::Matrix3;
-    use std::fs;
-    let filename = "SAC_GDY_V.msi";
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let atoms: Vec<Atom> = parser::msi_parser::parse_atom(&contents);
-    let lattice_vectors: Matrix3<f64> = parser::msi_parser::parse_lattice_vectors(&contents);
-    let filename_stem: &str = filename.split(".").collect::<Vec<&str>>()[0];
-    let lattice_molecule: Molecule = Molecule::new(filename_stem.to_string(), atoms);
-    let mut lat: Lattice = Lattice::new(lattice_molecule, lattice_vectors, vec![73], None);
-    lat.rotate_to_standard_orientation();
-    lat.set_adsorbate_name("C2H4".to_string());
-    match lat.get_adsorbate_name() {
-        Some(x) => println!("Adsorbate name: {}", x),
-        None => println!("There is no adsorbate in the lattice now."),
-    }
-    println!(
-        "Name: {}, number of atoms: {}",
-        lat.molecule.mol_name,
-        lat.molecule.number_of_atoms()
-    );
+fn task_gen_all() {
+    println!("Generate all base models");
+    editor::msi_editor::generate_all_base_models("GDY_tri.msi");
 }
