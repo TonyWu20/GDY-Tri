102ca48ce4c88f8906a3bf62ddeb963d7e71d029
TonyWu20 <tony.w21@gmail.com>
2022-06-10 23:44:27 +0800

    Test for sorting atoms

--- gdy_basic_model/src/test.rs
+++ gdy_basic_model/src/test.rs
@@ -20,4 +20,14 @@ mod test {
         let base_lat: Lattice = parser::msi_parser::parse_lattice(filename);
         println!("{}", base_lat.export_msi());
     }
+    #[test]
+    fn sort_atoms_in_cell() {
+        let filename = "./resources/GDY_tri.msi";
+        let base_lat: Lattice = parser::msi_parser::parse_lattice(filename);
+        let mut cell: Cell = Cell::new(base_lat, false);
+        cell.sort_atoms_by_elements();
+        for atom in cell.lattice.molecule.atoms_iterator() {
+            println!("{}", atom.element_name());
+        }
+    }
 }
