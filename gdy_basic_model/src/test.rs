87330b21253e73b464a5e28c00630e127af81b83
TonyWu20 <tony.w21@gmail.com>
2022-06-14 12:54:14 +0800

    Update tests

--- gdy_basic_model/src/test.rs
+++ gdy_basic_model/src/test.rs
@@ -1,8 +1,8 @@
 #[cfg(test)]
 mod test {
-    use gdy_model::*;
+    use gdy_model::{external_info::element_table, *};
 
-    use crate::parser;
+    use crate::{editor::msi_editor::change_atom_element, parser};
 
     // use crate::{editor, parser};
 
@@ -15,19 +15,41 @@ mod test {
     // }
 
     #[test]
+    #[ignore]
     fn read_and_write() {
         let filename = "./resources/GDY_tri.msi";
         let base_lat: Lattice = parser::msi_parser::parse_lattice(filename);
-        println!("{}", base_lat.export_msi());
+        println!("{}", base_lat.format_output());
     }
     #[test]
+    #[ignore]
     fn sort_atoms_in_cell() {
         let filename = "./resources/GDY_tri.msi";
-        let base_lat: Lattice = parser::msi_parser::parse_lattice(filename);
-        let mut cell: Cell = Cell::new(base_lat, false);
+        let mut base_lat: Lattice = parser::msi_parser::parse_lattice(filename);
+        println!("{:#?}", &base_lat.get_element_list());
+        let mut cell: Cell = Cell::new(&mut base_lat, false);
         cell.sort_atoms_by_elements();
-        for atom in cell.lattice.molecule.atoms_iterator() {
-            println!("{}", atom.element_name());
-        }
+    }
+    #[test]
+    #[ignore]
+    fn rotate_lattice() {
+        let filename = "./resources/GDY_tri.msi";
+        let mut base_lat: Lattice = parser::msi_parser::parse_lattice(filename);
+        base_lat.rotate_to_standard_orientation();
+        println!("{:#.12?}", base_lat.get_lattice_vectors());
+        println!("{}", base_lat.format_output());
+    }
+    #[test]
+    fn cell_test() {
+        let filename = "./resources/GDY_tri.msi";
+        let mut base_lat: Lattice = parser::msi_parser::parse_lattice(filename);
+        change_atom_element(base_lat.molecule.get_mut_atom_by_id(73).unwrap(), "V", 23);
+        // let frac_mat = fractional_coord_matrix(&base_lat);
+        let mut cell = Cell::new(&mut base_lat, false);
+        cell.sort_atoms_by_elements();
+        cell.lattice.rotate_to_standard_orientation();
+        let element_info = element_table::hash_table();
+        let cell_output = cell.format_output(&element_info);
+        println!("{}", cell_output);
     }
 }
