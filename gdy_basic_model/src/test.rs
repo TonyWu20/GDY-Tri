98c3fd2daddd52b60a1e9b90a7846996a19d5eb4
TonyWu20 <tony.w21@gmail.com>
2022-06-16 11:14:37 +0800

    Add param writing test

--- gdy_basic_model/src/test.rs
+++ gdy_basic_model/src/test.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod test {
-    use gdy_model::{external_info::element_table, *};
+    use crate::{external_info::element_table, param_writer::param_writer::write_param, *};
 
     use crate::{editor::msi_editor::change_atom_element, parser};
 
@@ -43,13 +43,36 @@ mod test {
     fn cell_test() {
         let filename = "./resources/GDY_tri.msi";
         let mut base_lat: Lattice = parser::msi_parser::parse_lattice(filename);
-        change_atom_element(base_lat.molecule.get_mut_atom_by_id(73).unwrap(), "V", 23);
+        change_atom_element(base_lat.molecule.get_mut_atom_by_id(73).unwrap(), "Mn", 25);
+        change_atom_element(base_lat.molecule.get_mut_atom_by_id(74).unwrap(), "Mn", 25);
+        change_atom_element(base_lat.molecule.get_mut_atom_by_id(75).unwrap(), "Ni", 28);
         // let frac_mat = fractional_coord_matrix(&base_lat);
         let mut cell = Cell::new(&mut base_lat, false);
         cell.sort_atoms_by_elements();
         cell.lattice.rotate_to_standard_orientation();
         let element_info = element_table::hash_table();
         let cell_output = cell.format_output(&element_info);
+        let spin_total = cell
+            .lattice
+            .molecule
+            .atoms_iterator()
+            .map(|atom| -> u8 { element_info.get(atom.element_name()).unwrap().spin })
+            .reduce(|total, x| total + x)
+            .unwrap();
         println!("{}", cell_output);
+        println!("{spin_total}");
+    }
+    #[test]
+    fn test_write_param() {
+        let filename = "./resources/GDY_tri.msi";
+        let mut base_lat: Lattice = parser::msi_parser::parse_lattice(filename);
+        change_atom_element(base_lat.molecule.get_mut_atom_by_id(73).unwrap(), "Mn", 25);
+        change_atom_element(base_lat.molecule.get_mut_atom_by_id(74).unwrap(), "Mn", 25);
+        change_atom_element(base_lat.molecule.get_mut_atom_by_id(75).unwrap(), "Ni", 28);
+        // let frac_mat = fractional_coord_matrix(&base_lat);
+        base_lat.update_base_name();
+        let cell = Cell::new(&mut base_lat, false);
+        let element_info = element_table::hash_table();
+        write_param(&cell, &element_info);
     }
 }
